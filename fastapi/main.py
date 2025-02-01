02c4eaba4391895f45fe897ea9dcecbaaeab9c78
Alex Kao <aexkao@gmail.com>
2025-02-01 18:29:11 -0500

    upload csv and write in local works

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,16 +1,30 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import csv
+import os
 
-
-from fastapi import FastAPI
-
-# `fastapi dev main.py` to run the server
 app = FastAPI()
 
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Allow only your Vue app's origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.post("/uploadfile/")
-async def upload_file():
-    return {"message": "File uploaded successfully"}
-
+async def upload_file(file: UploadFile = File(...)):
+    content = await file.read()
+    decoded_content = content.decode("utf-8").splitlines()
+    reader = csv.reader(decoded_content)
+    data = [row for row in reader]
+    file_location = f"uploaded_files/{file.filename}"
+    with open(file_location, "wb") as f:
+        f.write(content)
+    return {"filename": file.filename, "data": data}
