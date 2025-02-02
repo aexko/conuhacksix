cb98c45102eb391f5cb7cf6e3ef2e3d7adaa4e67
Alfred <fredblade142@gmail.com>
2025-02-01 21:46:18 -0500

    Calculation

--- alfred/matrixx.py
+++ alfred/matrixx.py
@@ -1,17 +1,23 @@
-import pandas as pd
-
-# Read the CSV data
-csv1 = 'outputIQA.csv'
-csv2 = 'outputTREES.csv'
-
-df1 = pd.read_csv(csv1)
-df2 = pd.read_csv(csv2)
-
-# Merge the DataFrames on the common column (e.g., 'date')
-merged_df = pd.merge(df1, df2, on='date')
-
-# Select only numeric columns for correlation calculation
-numeric_columns = merged_df.select_dtypes(include=['float64', 'int64']).columns
-correlation_matrix = merged_df[numeric_columns].corr()
-
-print(correlation_matrix)
+# import pandas as pd
+#
+#
+# def calculate_correlation(csv1, csv2):
+#     # Read the CSV data
+#     df1 = pd.read_csv(csv1)
+#     df2 = pd.read_csv(csv2)
+#
+#     # Merge the DataFrames on the common column (e.g., 'date')
+#     merged_df = pd.merge(df1, df2, on='date')
+#
+#     # Select only numeric columns for correlation calculation
+#     numeric_columns = merged_df.select_dtypes(include=['float64', 'int64']).columns
+#     correlation_matrix = merged_df[numeric_columns].corr()
+#
+#     return correlation_matrix
+#
+#
+# # Example usage
+# csv1 = 'outputIQA.csv'
+# csv2 = 'outputTREES.csv'
+# correlation_matrix = calculate_correlation(csv1, csv2)
+# print(correlation_matrix)
--- fastapi/main.py
+++ fastapi/main.py
@@ -1,5 +1,7 @@
+import json
 
-
+from modules.gemini import call_gemini_api
+from modules.calculation import calculate_correlation
 from fastapi import FastAPI
 
 # `fastapi dev main.py` to run the server
@@ -8,11 +10,28 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    # response = call_gemini_api()
+    #
+    # # print(response)
+    # try:
+    #     text_content = response["candidates"][0]["content"]["parts"][0]["text"]
+    #     json_string = text_content
+    #
+    #     # Step 2: Remove the Markdown code block syntax
+    #     json_string = json_string.strip("```json\n").strip("\n```")
+    #
+    #     # Step 3: Parse the JSON string into a Python object
+    #     parsed_data = json.loads(json_string)
+    #     return parsed_data
+    # except:
+    #     print('error')
+    #     return response
     return {"message": "Hello World"}
 
 @app.post("/uploadfile/")
 async def upload_file():
     return {"message": "File uploaded successfully"}
+
 
 @app.post("/merge")
 async def merge_files():
