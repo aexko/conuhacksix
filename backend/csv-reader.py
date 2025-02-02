547542349210f023644b62c55ac9ba088226aea2
Alfred <fredblade142@gmail.com>
2025-02-02 08:32:22 -0500

    Delete csv-reader.py

--- backend/csv-reader.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-
-def read_csv(file_path):
-    with open(file_path, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in spamreader:
-            print(', '.join(row))
